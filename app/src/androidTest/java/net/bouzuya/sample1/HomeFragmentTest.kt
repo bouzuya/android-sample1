279ddd4b8d24e42a8b5fba7063759a43f4123436
bouzuya <m@bouzuya.net>
2019-08-18 15:19:14 +0900

    use setTextByCount

--- app/src/androidTest/java/net/bouzuya/sample1/HomeFragmentTest.kt
+++ app/src/androidTest/java/net/bouzuya/sample1/HomeFragmentTest.kt
@@ -3,19 +3,27 @@ package net.bouzuya.sample1
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import org.junit.Test
 
 class HomeFragmentTest {
+    // TODO: mock viewModel
 
     @Test
     fun initialView() {
-        // TODO: mock viewModel
         launchFragmentInContainer<HomeFragment>()
 
         onView(withId(R.id.home_name_text_view)).check(matches(isDisplayed()))
         onView(withId(R.id.home_count_text_view)).check(matches(isDisplayed()))
         onView(withId(R.id.home_increment_button)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun initialValues() {
+        launchFragmentInContainer<HomeFragment>()
+
+        onView(withId(R.id.home_name_text_view)).check(matches(withText("Home")))
+        onView(withId(R.id.home_count_text_view)).check(matches(withText("0")))
+        onView(withId(R.id.home_increment_button)).check(matches(withText("+1")))
     }
 }
